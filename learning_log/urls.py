00f10b6f4a9af639fde6c6b27a0ed57ded9faf5a
zhao meng <1624849952@qq.com>
2019-09-27 10:43:27 +0800

    ''

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -18,5 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('learning_logs.urls', namespace='learning_logs')),
+    url(r'app/', include(('learning_logs.urls','learning_logs'),
+                     namespace='abc',
+                     )),
 ]
+
